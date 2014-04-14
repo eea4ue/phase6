181dcea54c34947658f5b3848a1fd868010dda39
eea4ue <eea4ue@virginia.edu>
2014-04-13 20:46:27 -0400

    Working Section Spinner
    Updates Section spinner with data that is dependent on the selection in
    Library Spinner. Each spinner starts with a blank “” value.

--- PhaseSix/src/com/cs4720project/phasesix/MeetingsFragment.java
+++ PhaseSix/src/com/cs4720project/phasesix/MeetingsFragment.java
@@ -7,12 +7,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MeetingsFragment extends Fragment {
+public class MeetingsFragment extends Fragment implements
+		OnItemSelectedListener {
 
 	LinearLayout spinners;
 	Button submitButton;
@@ -39,10 +43,39 @@ public class MeetingsFragment extends Fragment {
 		Context context = getActivity();
 		spinners = (LinearLayout) rootView.findViewById(R.id.spinners);
 
+		// LIBRARY SPINNER
 		libSpinner = (Spinner) rootView.findViewById(R.id.libSpinner);
+		// Create an ArrayAdapter using the string array and a default spinner
+		// layout
+		libSpinner.setOnItemSelectedListener((OnItemSelectedListener) this);
+		ArrayAdapter<CharSequence> libAdapter = ArrayAdapter
+				.createFromResource(context, R.array.lib_array,
+						android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		libAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		libSpinner.setAdapter(libAdapter);
+
 		sectionSpinner = (Spinner) rootView.findViewById(R.id.sectionSpinner);
+		
+		// TIME SPINNER
 		timeSpinner = (Spinner) rootView.findViewById(R.id.timeSpinner);
+		ArrayAdapter<CharSequence> timeAdapter = ArrayAdapter
+				.createFromResource(context, R.array.time_array,
+						android.R.layout.simple_spinner_item);
+		timeAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		timeSpinner.setAdapter(timeAdapter);
+
+		// DAY SPINNER
 		daySpinner = (Spinner) rootView.findViewById(R.id.daySpinner);
+		ArrayAdapter<CharSequence> dayAdapter = ArrayAdapter
+				.createFromResource(context, R.array.day_array,
+						android.R.layout.simple_spinner_item);
+		dayAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		daySpinner.setAdapter(dayAdapter);
 
 		// try {
 		// Log.d("searchTerm", userViewCoursesURL);
@@ -84,6 +117,77 @@ public class MeetingsFragment extends Fragment {
 			tempWorkAround = true;
 			Log.d("from setUser in SchedulesFrag", userName);
 		}
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+
+		if (arg0.equals(libSpinner)) {
+				sectionSpinner.setEnabled(true);
+
+		if (libSpinner.getSelectedItem().equals("Alderman")) {
+			ArrayAdapter<CharSequence> adapter0 = ArrayAdapter
+					.createFromResource(getActivity(), R.array.alderman_array,
+							android.R.layout.simple_spinner_item);
+			adapter0.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			sectionSpinner.setAdapter(adapter0);
+
+		} else if (libSpinner.getSelectedItem().equals("Clark")) {
+
+			ArrayAdapter<CharSequence> adapter1 = ArrayAdapter
+					.createFromResource(getActivity(), R.array.clark_array,
+							android.R.layout.simple_spinner_item);
+			adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			sectionSpinner.setAdapter(adapter1);
+		} else if (libSpinner.getSelectedItem().equals("Clemons")) {
+
+			ArrayAdapter<CharSequence> adapter2 = ArrayAdapter
+					.createFromResource(getActivity(), R.array.clemons_array,
+							android.R.layout.simple_spinner_item);
+			adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			sectionSpinner.setAdapter(adapter2);
+		} else if (libSpinner.getSelectedItem().equals("Commerce School")) {
+
+			ArrayAdapter<CharSequence> adapter3 = ArrayAdapter
+					.createFromResource(getActivity(),
+							R.array.commerce_school_array,
+							android.R.layout.simple_spinner_item);
+			adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			sectionSpinner.setAdapter(adapter3);
+		}
+
+		else if (libSpinner.getSelectedItem().equals("Rice")) {
+
+			ArrayAdapter<CharSequence> adapter4 = ArrayAdapter
+					.createFromResource(getActivity(), R.array.rice_array,
+							android.R.layout.simple_spinner_item);
+			adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			sectionSpinner.setAdapter(adapter4);
+		} else if (libSpinner.getSelectedItem().equals("Thornton")) {
+
+			ArrayAdapter<CharSequence> adapter5 = ArrayAdapter
+					.createFromResource(getActivity(), R.array.thornton_array,
+							android.R.layout.simple_spinner_item);
+			adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			sectionSpinner.setAdapter(adapter5);
+		} else if (libSpinner.getSelectedItem().equals("Wilsdorf")) {
+
+			ArrayAdapter<CharSequence> adapter6 = ArrayAdapter
+					.createFromResource(getActivity(), R.array.wilsdorf_array,
+							android.R.layout.simple_spinner_item);
+			adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			sectionSpinner.setAdapter(adapter6);
+		}
+		// default:
+		// return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// nothing
+
 	}
 
 	// private class GetUserCoursesTask extends AsyncTask<String, Void, String>
